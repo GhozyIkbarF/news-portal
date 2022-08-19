d140593c76ed95136f0b5be07bf57ad7f0c0810d
MuhammadAsharul <masharul51@gmail.com>
2022-08-19 21:14:36 +0700

    perubahan navbar

--- resources/js/Components/Navbar/index.jsx
+++ resources/js/Components/Navbar/index.jsx
@@ -156,7 +156,7 @@ export default function index() {
                 <div className="px-2 pt-2 pb-3 space-y-1">
                     <a
                         href="#"
-                        className=" text-white block px-3 py-2 rounded-md text-base font-medium"
+                        className=" text-gray-300 block px-3 py-2 rounded-md text-base font-medium"
                         aria-current="page"
                     >
                         Dashboard
