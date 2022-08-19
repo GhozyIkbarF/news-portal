4c6575ca25dac53a4f9b2c58832a1a683db44f4f
GhozyIkbarF <ghozyikbarf@gmail.com>
2022-08-19 21:48:12 +0700

    change link navbar

--- resources/js/Components/Navbar/index.jsx
+++ resources/js/Components/Navbar/index.jsx
@@ -71,33 +71,33 @@ export default function index() {
                         </div>
                         <div className="hidden sm:block sm:ml-6">
                             <div className="flex space-x-4">
-                                <a
-                                    href="#"
+                                <Link
+                                    to="/"
                                     className="text-gray-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
                                 >
                                     Stories
-                                </a>
+                                </Link>
 
-                                <a
-                                    href="#"
+                                <Link
+                                    to="/"
                                     className="text-gray-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
                                 >
                                     Forum
-                                </a>
+                                </Link>
 
-                                <a
-                                    href="#"
+                                <Link
+                                    to="/"
                                     className="text-gray-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
                                 >
                                     Store
-                                </a>
+                                </Link>
 
-                                <a
-                                    href="#"
+                                <Link
+                                    to="/"
                                     className="text-gray-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-bold"
                                 >
                                     Contact
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -153,40 +153,40 @@ export default function index() {
 
             <div className="sm:hidden" id="mobile-menu">
                 <div className="px-2 pt-2 pb-3 space-y-1">
-                    <a
-                        href="#"
+                    <Link
+                        to="/"
                         className=" text-gray-500 block px-3 py-2 rounded-md text-base font-medium"
                         aria-current="page"
                     >
                         Dashboard
-                    </a>
+                    </Link>
 
-                    <a
-                        href="#"
+                    <Link
+                        to="/"
                         className="text-gray-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                     >
                         Stories
-                    </a>
+                    </Link>
 
-                    <a
-                        href="#"
+                    <Link
+                        to="/"
                         className="text-gray-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                     >
                         Forum
-                    </a>
+                    </Link>
 
-                    <a
-                        href="#"
+                    <Link
+                        to="/"
                         className="text-gray-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                     >
                         Store
-                    </a>
-                    <a
-                        href="#"
+                    </Link>
+                    <Link
+                        to="/"
                         className="text-gray-500 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                     >
                         Contact
-                    </a>
+                    </Link>
                 </div>
             </div>
         </nav>
--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "../Components/Navbar/index";
+
 
 export default function Homepage(props) {
     console.log(props)
-    return(
-        <div className="flex flex-col justify-center align-center">
-            <Navbar />
-            <div className="flex justify-center align-center">
-                <h1>{props.title}</h1>
-                <p>{props.description}</p>
+    return (
+        <BrowserRouter>
+            <div className="flex flex-col justify-center align-center">
+                <Navbar />
+                {/* <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                </Routes> */}
+                <div className="flex justify-center align-center">
+                    <h1>{props.title}</h1>
+                    <p>{props.description}</p>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
+
     )
 }
